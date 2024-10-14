0393c426adba3718620b52af3c53308894c7ff97
IlmanHanifa <ilmanhanifa9@gmail.com>
2024-10-14 14:12:10 +0700

    #5 Object Type

---
+++ OOP/objecttype.php
@@ -0,0 +1,39 @@
+
+<?php
+
+class ProdukObjectType
+{
+    public $judul, $penulis, $penerbit, $harga;
+
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+class CetakInfoProduk
+{
+    public function cetak(ProdukObjectType $produk)
+    {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 = new ProdukObjectType("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+$produk2 = new ProdukObjectType("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
